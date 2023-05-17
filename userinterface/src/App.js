34e3757bdf614a88fd3bbf486c77b28f948b2384
Omar Amin <oamin.omar@gmail.com>
2023-05-17 15:56:29 +0300

    add results page route

--- userinterface/src/App.js
+++ userinterface/src/App.js
@@ -1,6 +1,7 @@
 import Footer from "./components/Footer";
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
+import Results from "./pages/Results";
 import { Switch, Routes, Route } from "react-router-dom"
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} exact></Route>
+        <Route path="/results" element={<Results />} exact></Route>
       </Routes>
       <Footer />
     </div>
