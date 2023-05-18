4fec65164f882eaa1873e3c1e8ba21a04b3f2fd9
Omar Amin <oamin.omar@gmail.com>
2023-05-18 04:59:36 +0300

    search box now suggests

--- userinterface/src/components/Layout.js
+++ userinterface/src/components/Layout.js
@@ -5,13 +5,22 @@ import classes from './style/layout.module.css'
 import ArrowRightAltIcon from '@mui/icons-material/ArrowForward';
 import duck from '../img/duck.png'
 import ducky2 from '../img/giphy.gif'
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+
 import axios from 'axios'
+import { useEffect } from 'react';
 
 const Layout = () => {
   const [search, setSearch] = useState('')
   const [results, setResults] = useState([])
+  const [previosSearch, setPreviousSearch] = useState([])
+  const [suggest, setSuggest] = useState([])
   const navigate = useNavigate();
   let audio = new Audio("/DuckQuack.mp3")
+
+
+
 
   function handleEnter(e){
     if(e.key==='Enter'){
@@ -19,30 +28,33 @@ const Layout = () => {
       sendSearch()
     }
   }
-  
+  useEffect(() => {
+    if(sessionStorage.getItem('previous')!==null){
+      setSuggest(sessionStorage.getItem('previous').split(','))
+    }
+    if(sessionStorage.getItem('previous')===null){
+      sessionStorage.setItem('previous',"Quack")
+    }
+    
+  }, [])
+
   function sendSearch(){
     audio.play()
     if(search===''){
       return
     }
+    setPreviousSearch(...previosSearch,sessionStorage.getItem('previous'))
+    
+    sessionStorage.setItem('previous',sessionStorage.getItem('previous')+","+search)
+
     navigate("/results/"+search);
   }
  
-  // function searchDuck(){
-  //   console.log(search)
-
-  //   axios.get("http://localhost:8080/words?q=plane"+search)
-  //   .then(res=>{
-  //     console.log(res)
-  //     setResults(res.data)
-  //   }
-  //   )
-  //   .catch(err=>console.log(err))
-
-  // }
-
-
-
+ function handleChange(e){
+    setSearch(e.target.value)
+    console.log(e.target.value)
+  }
+  // (e)=>setSearch(e.target.value)
 
   return (
     <div className={classes.layout}>
@@ -50,7 +62,25 @@ const Layout = () => {
         <div className={classes.searchBoxContainer}>
           <div className={classes.searchBox}>
             <img src={ducky2} alt='duck'/>
-            <input onKeyDown={handleEnter} type='text' placeholder='Search Quacky' onChange={(e)=>setSearch(e.target.value)} />
+            {/* <input onKeyDown={handleEnter} type='text' placeholder='Search Quacky' onChange={(e)=>setSearch(e.target.value)} /> */}
+                <Autocomplete
+
+                    inputValue={search}
+                    onInputChange={(event, newInputValue) => {
+                      setSearch(newInputValue);
+                    }}
+                    onKeyDown={handleEnter}
+                    sx={{ "width": "70rem", "paddinLeft": "2rem",
+                     "backgroundColor": "rgba(250, 235, 215, 0.853)"
+                     , "borderRadius": "10px",
+                      border: "none",
+                      }}
+                    id="free-solo-demo"
+                    freeSolo
+                    options={suggest.map((option) => option)}
+                    renderInput={(params) => <TextField {...params} label="Search Quacky" />}
+                  />
+                 
             <button onClick={sendSearch} >
               Quack Quack
               <ArrowRightAltIcon style={{fontSize:25}} />
